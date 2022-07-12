bdda0314241e5d16ee63e5cedfc71fce6c18d734
Gedewon Haile <gedewon.hayle.9@gmail.com>
2022-07-12 23:06:19 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,3 @@
-/* Populate database with sample data. */
-
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES ('Agumon'  , '2020-02-03', 0, true , 10.23),
 	     ('Gabumon' , '2018-11-15', 2, true , 8),
