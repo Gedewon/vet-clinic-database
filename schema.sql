9ea2b3c692b24c8893b13371031c477f639e6b48
gedewon <gedewon.hayle.9@gmail.com>
2022-07-13 21:25:46 +0300

    Create owners and species schema

--- schema.sql
+++ schema.sql
@@ -12,3 +12,34 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(100);
+
+
+CREATE TABLE owners (
+   id         INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+   full_name  VARCHAR(100),
+   age        INT,
+
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+  id          INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name        VARCHAR(100),
+
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT,
+ADD CONSTRAINT fk_species_id
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals ADD COLUMN owners_id INT,
+ADD CONSTRAINT fk_owners_id
+FOREIGN KEY (owners_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
