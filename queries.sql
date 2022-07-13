92d8c16d721c96644f5b69e92f1d3972056cf0fe
gedewon <gedewon.hayle.9@gmail.com>
2022-07-13 18:13:12 +0300

    Add transaction Before 2022

--- queries.sql
+++ queries.sql
@@ -32,3 +32,11 @@ DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT BEFORE2022;
+UPDATE animals SET weight_kg = (weight_kg*-1);
+ROLLBACK TO BEFORE2022;
+UPDATE animals SET weight_kg = (weight_kg*-1) WHERE weight_kg < 0;
+COMMIT;
